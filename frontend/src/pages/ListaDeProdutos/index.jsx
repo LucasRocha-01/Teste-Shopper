4aff60e2c3ecadde8445bd8117fc0314c5cb2371
Lucas <48070334+LucasRocha-01@users.noreply.github.com>
2023-05-15 22:14:59 -0300

    feat tratativa para sem produtos

--- frontend/src/pages/ListaDeProdutos/index.jsx
+++ frontend/src/pages/ListaDeProdutos/index.jsx
@@ -1,5 +1,5 @@
 import { useProdutos } from '../../data/hooks/useProdutos'
-import { Container } from './style'
+import { AvisoNoData, Container } from './style'
 import Button from '../../assets/components/button'
 import { TableProducts, TrProducts } from '../../assets/global_style'
 
@@ -10,40 +10,52 @@ const ListaDeProdutos = () => {
     <Container>
       <span>Shopper - Dashboard</span>
       <h1>Lista de Produtos</h1>
-      <div className="card">
-        <TableProducts>
-          <thead>
-            <tr>
-              <th>Codigo</th>
-              <th>Nome</th>
-              <th>Preço de Custo</th>
-              <th>Preço de Venda</th>
-            </tr>
-          </thead>
-          <tbody>
-            {produtos.map((produto) => (
-              <TrProducts key={produto.code}>
-                <td>{produto.code}</td>
-                <td>{produto.name}</td>
-                <td>
-                  R${' '}
-                  {produto.cost_price.toLocaleString('pt-BR', {
-                    minimumFractionDigits: 2,
-                    maximumFractionDigits: 2,
-                  })}
-                </td>
-                <td>
-                  R${' '}
-                  {produto.sales_price.toLocaleString('pt-BR', {
-                    minimumFractionDigits: 2,
-                    maximumFractionDigits: 2,
-                  })}
-                </td>
-              </TrProducts>
-            ))}
-          </tbody>
-        </TableProducts>
-      </div>
+
+      {produtos.length > 0 ? (
+        <div>
+          <TableProducts>
+            <thead>
+              <tr>
+                <th>Codigo</th>
+                <th>Nome</th>
+                <th>Preço de Custo</th>
+                <th>Preço de Venda</th>
+              </tr>
+            </thead>
+            <tbody>
+              {produtos.map((produto) => (
+                <TrProducts key={produto.code}>
+                  <td>{produto.code}</td>
+                  <td>{produto.name}</td>
+                  <td>
+                    R${' '}
+                    {produto.cost_price.toLocaleString('pt-BR', {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}
+                  </td>
+                  <td>
+                    R${' '}
+                    {produto.sales_price.toLocaleString('pt-BR', {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}
+                  </td>
+                </TrProducts>
+              ))}
+            </tbody>
+          </TableProducts>
+        </div>
+      ) : (
+        <AvisoNoData>
+          <h2>Sem Dados</h2>
+          <p>
+            Opa, você ainda não tem dados no seu banco.
+            <br /> Volte quando subir
+          </p>
+        </AvisoNoData>
+      )}
+
       <Button to={'/upload'} text={'Subir CSV Atualizado'} />
     </Container>
   )
--- frontend/src/pages/ListaDeProdutos/style.js
+++ frontend/src/pages/ListaDeProdutos/style.js
@@ -3,3 +3,8 @@ import styled from 'styled-components'
 export const Container = styled.div`
   color: #000000;
 `
+export const AvisoNoData = styled.div`
+  background: #ff9292;
+  border-radius: 10px;
+  padding: 20px;
+`
